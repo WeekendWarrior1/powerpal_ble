522c3b6d27f3e65bc2a42458c3a3b542822409b0
WeekendWarrior1 <c.halton.93@gmail.com>
2022-05-09 10:57:07 +1000

    Option to validate apikey using http

--- auth_extraction/retrieve_api_key.py
+++ auth_extraction/retrieve_api_key.py
@@ -1,7 +1,7 @@
 from encodings import utf_8
 import sys
 import asyncio
-import logging
+import requests
 
 from bleak import BleakClient, BleakScanner, BleakError
 
@@ -25,34 +25,46 @@ async def main(address, my_pairing_code):
         if not (0 <= my_pairing_code <= 999999):
             my_pairing_code = None
             print("Pairing Code should be 6 digits...")
+    input("Please confirm that you are NOT connected to the Powerpal via Bluetooth using any devices, and hit enter to continue...")
 
     async with BleakClient(address) as client:
-        logger.info(f"Connected: {client.is_connected}")
+        print(f"Connected: {client.is_connected}")
 
         paired = await client.pair(2)
-        logger.info(f"Paired?: {paired}")
+        print(f"Paired?: {paired}")
 
-        logger.info(f"Authenticating with pairing_code: {my_pairing_code}, converted: {convert_pairing_code(my_pairing_code)}")
+        print(f"Authenticating with pairing_code: {my_pairing_code}, converted: {convert_pairing_code(my_pairing_code)}")
         await client.write_gatt_char(pairingCodeChar, convert_pairing_code(my_pairing_code), response=False)
-        logger.info('Auth Success\n')
+        print('Auth Success\n')
 
         await asyncio.sleep(0.5)
 
-        logger.info(f"Attempting to retrieve Powerpal Serial Number...")
+        print(f"Attempting to retrieve Powerpal Serial Number...")
         serial = await client.read_gatt_char(powerpalSerialChar)
         formatted_serial = ''.join('{:02x}'.format(x) for x in reversed(serial)).lower()
-        logger.info(f"Retrieved Device Serial:   {formatted_serial}")
-        logger.info(f"Meaning your meter reading endpoint is:  https://readings.powerpal.net/api/v1/meter_reading/{formatted_serial}\n")
+        print(f"Retrieved Device Serial:   {formatted_serial}")
+        print(f"Meaning your meter reading endpoint is:  https://readings.powerpal.net/api/v1/meter_reading/{formatted_serial}\n")
 
-        logger.info(f"Attempting to retrieve apikey (Powerpal UUID)...")
+        print(f"Attempting to retrieve apikey (Powerpal UUID)...")
         apikey = await client.read_gatt_char(powerpalUUIDChar)
         joined_apikey = ''.join('{:02x}'.format(x) for x in apikey)
         formatted_apikey = f"{joined_apikey[:8]}-{joined_apikey[8:12]}-{joined_apikey[12:16]}-{joined_apikey[16:20]}-{joined_apikey[20:]}".lower()
-        logger.info(f"Retrieved apikey: {formatted_apikey}")
+        print(f"Retrieved apikey: {formatted_apikey}\n")
 
-        # while True:
-        #     await asyncio.sleep(0.1)
+        test_endpoint = input(f"Would you like this script to validate your apikey by using it to make a request for your device information from https://readings.powerpal.net/api/v1/device/{formatted_serial}? (y/n)")
+        if test_endpoint.lower().startswith('y'):
+            url = f"https://readings.powerpal.net/api/v1/device/{formatted_serial}"
+            headers = { 'Authorization': formatted_apikey }
+
+            response = requests.request("GET", url, headers=headers, data=payload)
+            if (response.status_code < 300):
+                print("Success! Looks like your Device Serial and apikey were succesfully retrieved! Here is the device information returned from https://readings.powerpal.net/api/ :")
+                print(response.text)
+            else: 
+                print("Failure!")
+                print(f"{response.status_code} - {response.reason} - {response.text}")
+        else:
+            print(f'You may also confirm your apikey by using curl:\ncurl -H "Authorization: {formatted_apikey}" https://readings.powerpal.net/api/v1/device/{formatted_serial}')
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     asyncio.run(main((sys.argv[1] if len(sys.argv) >= 2 else None),(sys.argv[2] if len(sys.argv) == 3 else None)))
